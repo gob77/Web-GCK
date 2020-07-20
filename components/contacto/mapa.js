6637cf21b4e942cf77aa9e78f1afb0976c582050
gob77 <mraortiz@hotmail.com>
2020-07-20 20:59:17 -0300

    arreglos menores

--- components/contacto/mapa.js
+++ components/contacto/mapa.js
@@ -1,9 +1,7 @@
-const map = document.getElementById("map");
-
-console.log(map);
+const mapa = document.getElementById("map");
 
 function initMap() {
-    var uluru = { lat: -31.470556, lng: -64.1875 };
-    var map = new google.maps.Map(document.getElementById("map"), { zoom: 18, center: uluru });
-    var marker = new google.maps.Marker({ position: uluru, map: map });
+    var karabitian = { lat: -31.470556, lng: -64.1875 };
+    var map = new google.maps.Map(mapa, { zoom: 16, center: karabitian });
+    var marker = new google.maps.Marker({ position: karabitian, map: map });
 }
