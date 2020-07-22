22666377d2592a89a6357e9113d160632a021d27
gob77 <mraortiz@hotmail.com>
2020-07-22 05:09:47 -0300

    añadiendo media querys

--- script.js
+++ script.js
@@ -1,45 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
     let body = document.getElementsByTagName("body")[0];
 
-    console.log(window.innerWidth);
-
-    window.addEventListener("resize", () => {
-        console.log(window.innerWidth);
-    });
-
-    $(function () {
-        $(".multiple-items").slick({
-            infinite: true,
-            slidesToShow: 6,
-            slidesToScroll: 1,
-            arrows: false,
-            autoplay: true,
-            autoplaySpeed: 2000,
-            variableWidth: true,
-            adaptiveHeight: true,
-            responsive: [
-                {
-                    breakpoint: 768,
-                    settings: {
-                        arrows: false,
-                        centerMode: true,
-                        slidesToShow: 3,
-                        autoplay: true,
-                        autoplaySpeed: 2000,
-                    },
+    $(".responsive").slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        autoplay: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true,
                 },
-                {
-                    breakpoint: 425,
-                    settings: {
-                        arrows: false,
-                        centerMode: false,
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                        autoplay: true,
-                        autoplaySpeed: 2000,
-                    },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
                 },
-            ],
-        });
+            },
+            {
+                breakpoint: 425,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ],
     });
 });
