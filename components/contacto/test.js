be3ea0ad1fc0f50f5c8d99d635eddfc2f65ae074
gob77 <mraortiz@hotmail.com>
2020-07-28 17:28:52 -0300

    test btn contacto

---
+++ components/contacto/test.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let test = document.getElementsByClassName("test");
+    let btn = document.getElementById("btn");
+
+    for (let i = 0; i < test.length; i++) {
+        test[i].style.visibility = "hidden";
+    }
+
+    btn.parentNode.style.backgroundColor = "red";
+});
