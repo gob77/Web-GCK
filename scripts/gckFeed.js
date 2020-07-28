25ba34818606acc004f0631e7bdec55e2423665b
gob77 <mraortiz@hotmail.com>
2020-07-28 08:56:31 -0300

    fix menores

--- scripts/gckFeed.js
+++ scripts/gckFeed.js
@@ -1,4 +1,4 @@
-/* (function () {
+(function () {
     new InstagramFeed({
         username: "grupocarloskarabitian",
         container: document.getElementById("gck__feed"),
@@ -12,4 +12,3 @@
         margin: 0.1,
     });
 })();
- */
